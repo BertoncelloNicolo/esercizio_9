27cf80e212d6c903a07e3307a1bf564df0858496
BertoncelloNicolo <98811695+BertoncelloNicolo@users.noreply.github.com>
2023-10-17 18:05:41 +0200

    Add files via upload

---
+++ server.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define SERVER_PORT 40000
+#define SOCKET_ERROR ((int)-1)
+#define DIMBUFF 512
+
+int main(int argc, char *argv[])
+{
+
+    struct sockaddr_in servizio, rem_indirizzo;
+    int pid, nread, soa, socketfd, client_len, fd, on = 1, fromlen = sizeof(servizio);
+
+    memset((char *)&servizio, 0, sizeof(servizio));
+
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVER_PORT);
+
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Bind
+    setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    bind(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+
+    listen(socketfd, 10);
+
+    // attesa del client
+    for (;;)
+    {
+        printf("\n\nServer in ascolto...\n");
+        fflush(stdout);
+
+        soa = accept(socketfd, (struct sockaddr *)&rem_indirizzo, &fromlen);
+
+        // pid = fork();
+
+        if (pid == 0)
+        {
+            char nome_file[256];
+            ssize_t nread = read(soa, nome_file, sizeof(nome_file));
+
+            if (nread < 0)
+            {
+                perror("Errore nella lettura del nome del file");
+                close(soa);
+                exit(1);
+            }
+
+            nome_file[nread] = '\0';
+            printf("Richiesta del file: %s\n", nome_file);
+
+            // Chiudi il socket inutilizzato
+            close(socketfd);
+
+            // Redirect dell'output del processo figlio verso il socket
+            if (dup2(soa, 1) == -1)
+            {
+                perror("Errore nel redirect dell'output verso il socket");
+                close(soa);
+                exit(1);
+            }
+
+            close(soa);
+
+            // Eseguire il comando cat con il nome del file
+            if (execl("/bin/cat", "cat", nome_file, NULL) == -1)
+            {
+                perror("Errore nell'esecuzione di execl");
+                exit(1);
+            }
+        }
+
+        close(socketfd);
+    }
+
+    return 0;
+}
